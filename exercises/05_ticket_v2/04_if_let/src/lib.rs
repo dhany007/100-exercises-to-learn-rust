5e10162f4e85dcce97ccdacfe6cab365161db483
dhany <dhany@citra-digital.org>
2025-12-12 14:14:45 +0700

    05_ticket_v2 - 03_variants_with_data

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,18 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        // using if let
+        // if let Shape::Circle { radius } = self {
+        //     *radius
+        // } else {
+        //     panic!("only circle have radius")
+        // }
+
+        // using let else
+        let Shape::Circle { radius } = self else {
+            panic!("only circle have radius")
+        };
+        *radius
     }
 }
 
