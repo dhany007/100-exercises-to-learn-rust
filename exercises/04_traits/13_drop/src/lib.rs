a45656f1d32ecd1e7745b914bfd8dc804223df3b
dhany <dhany@citra-digital.org>
2025-12-12 10:39:28 +0700

    04_traits - 13_drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,31 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+#[derive(Debug)]
+pub struct DropBomb{
+    defused: bool
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("DropBomb was dropped without being defused")
+        }
+    }
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb {
+        DropBomb { 
+            defused: false
+         }
+    }
+
+    pub fn defuse(&mut self) {
+        self.defused = true
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
